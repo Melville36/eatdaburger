85822d6dade4950b44ab523e2084eba60c3dbba0
Melvin Hill <melvinhill.861@gmail.com>
2017-08-24 00:01:30 -0400

    Final Burger App

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,15 @@
--- Create the database day_planner_db and specified it for use.
+-- Create and use 
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
 -- Create the table plans.
-CREATE TABLE movies
+CREATE TABLE burgers
 (
 id int NOT NULL AUTO_INCREMENT,
-movie varchar(255) NOT NULL,
+burger_name varchar(255) NOT NULL,
+devoured BOOLEAN DEFAULT false,
+date TIMESTAMP,
 PRIMARY KEY (id)
 );
 
--- Insert a set of records.
-INSERT INTO movies (movie) VALUES ('Titanic');
-INSERT INTO movies (movie) VALUES ('Remember the Titans');
-INSERT INTO movies (movie) VALUES ('Friday');
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
