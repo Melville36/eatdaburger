f6286c22125549c6cd2b1404c37519886dc8dfb9
Melvin Hill <melvinhill.861@gmail.com>
2017-08-24 21:17:22 -0400

    change

--- config/connection.js
+++ config/connection.js
@@ -10,13 +10,18 @@ var connection = mysql.createConnection({
 });
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+    connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+}
+
 
 // Export connection for our ORM to use.
 module.exports = connection;
